0bac8501bd74a500f45a5d7754ceb90bd8d3b530
Abhinav Shah <abhinavk.shah@gmail.com>
2018-04-24 12:00:11 +0530

    File Modified And Indented Properly.

--- request_handler.php
+++ request_handler.php
@@ -11,46 +11,52 @@ include('libraries/headers.php');
 
 if(isset($_POST['login']))
     include('libraries/login.php');
+    
 else if(isset($_POST['addCategory']))
     include('libraries/addCategory.php');
 else if(isset($_GET['viewCategory']))
     include('libraries/viewCategory.php');
-else if(isset($_POST['addSubCategory']))
-    include('libraries/addSubCategory.php');
-else if(isset($_GET['viewSubCategory']))
-    include('libraries/viewSubCategory.php');
-else if(isset($_POST['addSkill']))
-    include('libraries/addSkill.php');
-else if(isset($_GET['viewSkill']))
-    include('libraries/viewSkill.php');
-else if(isset($_GET['viewCategoryData']))
-    include('libraries/viewCategoryData.php');    
 else if(isset($_POST['updateCategory']))
     include('libraries/updateCategory.php');
 else if(isset($_POST['deleteCategory']))
     include('libraries/deleteCategory.php');
-else if(isset($_GET['viewSubCategoryData']))
-    include('libraries/viewSubCategoryData.php');      
+
+else if(isset($_POST['addSubCategory']))
+    include('libraries/addSubCategory.php');
+else if(isset($_GET['viewSubCategory']))
+    include('libraries/viewSubCategory.php');
 else if(isset($_POST['updateSubCategory']))
     include('libraries/updateSubCategory.php');    
 else if(isset($_POST['deleteSubCategory']))
     include('libraries/deleteSubCategory.php');
-else if(isset($_GET['viewSkillData']))
-    include('libraries/viewSkillData.php');    
+
+else if(isset($_POST['addSkill']))
+    include('libraries/addSkill.php');
+else if(isset($_GET['viewSkill']))
+    include('libraries/viewSkill.php');
 else if(isset($_POST['updateSkill']))
     include('libraries/updateSkill.php');
 else if(isset($_POST['deleteSkill']))
     include('libraries/deleteSkill.php');
+
 else if(isset($_POST['addForm']))
     include('libraries/addForm.php'); 
 else if(isset($_GET['viewForm']))
     include('libraries/viewForm.php');
-else if(isset($_GET['viewFormData']))
-    include('libraries/viewFormData.php');
 else if(isset($_POST['updateForm']))
     include('libraries/updateForm.php'); 
 else if(isset($_POST['deleteForm']))
     include('libraries/deleteForm.php');
+
+else if(isset($_GET['viewCategoryData']))
+    include('libraries/viewCategoryData.php');    
+else if(isset($_GET['viewSubCategoryData']))
+    include('libraries/viewSubCategoryData.php');      
+else if(isset($_GET['viewSkillData']))
+    include('libraries/viewSkillData.php');    
+else if(isset($_GET['viewFormData']))
+    include('libraries/viewFormData.php');
+
 else if(isset($_POST['addQuestion']))
     include('libraries/addQuestion.php');
 else if(isset($_GET['viewQuestions']))
